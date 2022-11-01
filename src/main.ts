feef8b1de9aef829a804769124f0c73884b7f868
Flavian Engevin <flavian.engevin@ynov.com>
2022-11-01 17:37:20 +0100

    Update docker compose + setup base TypeORM

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import { NestFactory } from "@nestjs/core";
+import { NestFactory } from '@nestjs/core';
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
-import { AppModule } from "./app.module";
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { AppModule } from './app.module';
+
+console.log('ICI', process.env.NODE_ENV);
 
 import { AppModule } from '@modules/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
-  await app.listen(3000);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+    await app.listen(3000);
 }
 bootstrap();
