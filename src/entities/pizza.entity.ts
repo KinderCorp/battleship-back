1e0686c4298d1383457b9199aa72e0530b78a211
Flavian Engevin <flavian.engevin@ynov.com>
2022-11-03 16:53:52 +0100

    Cu 376awza Setup Postgres (#4)
    * Create docker-compose
    
    * Format and use fastify instead express
    
    * Update docker compose + setup base TypeORM
    
    * Add pg
    
    * Fix config
    
    * Updates paths

---
+++ src/entities/pizza.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Pizza {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column('text', { array: true })
+    ingredient: string[];
+
+    @Column()
+    isVegetarian: boolean;
+}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,31 @@
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from '@controllers/app.controller';
 import { AppService } from '@services/app.service';
+import { Pizza } from '@entities/pizza.entity';
 
 @Module({
     controllers: [AppController],
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ['.env.local', '.env', '.env.production'],
+        }),
+        TypeOrmModule.forRoot({
+            database: process.env.POSTGRESQL_DATABASE,
+            entities: [Pizza],
+            host: process.env.POSTGRESQL_HOST,
+            password: process.env.POSTGRESQL_PASSWORD,
+            port: Number(process.env.POSTGRESQL_PORT),
+            synchronize: process.env.POSTGRESQL_HOST === 'localhost',
+            type: 'postgres',
+            username: process.env.POSTGRESQL_USER,
+        }),
+    ],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private dataSource: DataSource) {}
+}
