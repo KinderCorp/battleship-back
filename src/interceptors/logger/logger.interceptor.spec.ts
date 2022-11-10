d334c3299171def12ce325f73ce758e8490dd346
Maxime <maxime.warluzel@ynov.com>
2022-11-10 09:55:02 +0100

    Fix logger tests

--- src/interceptors/logger/logger.interceptor.spec.ts
+++ src/interceptors/logger/logger.interceptor.spec.ts
@@ -1,7 +1,12 @@
+import {
+  Logger,
+  LoggerService,
+} from '@nestjs/common';
 import { LoggerInterceptor } from './logger.interceptor';
 
 describe('LoggerInterceptor', () => {
   it('should be defined', () => {
-    expect(new LoggerInterceptor()).toBeDefined();
+    const logger: LoggerService = Logger;
+    expect(new LoggerInterceptor(logger)).toBeDefined();
   });
 });
