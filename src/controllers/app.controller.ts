21f2eea618ac3a84662aadeb2b433be136d307d3
Flavian Engevin <flavian.engevin@ynov.com>
2022-11-13 16:12:49 +0100

    Add an error handler

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,5 +1,5 @@
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
 
 import { AppService } from '@services/app.service';
 @ApiTags('Test route')
@@ -7,8 +7,15 @@ import { AppService } from '@services/app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
+  @Get('hello/:lang')
   @ApiOperation({ summary: 'Test routing with an "Hello World"' })
+  @ApiParam({
+    allowEmptyValue: true,
+    description:
+      "The language of the 'hello'. Default is english. 'ru' will send an error. ",
+    enum: ['fr', 'en', 'ru'],
+    name: 'lang',
+  })
   @ApiResponse({ description: 'OK', status: 200 })
   getHello(): Record<string, string> {
     return this.appService.getHello();
