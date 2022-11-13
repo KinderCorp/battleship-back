2635aacbbe051f7f3322edc495ff57d7e6b27e6e
Flavian Engevin <flavian.engevin@ynov.com>
2022-11-13 16:27:33 +0100

    SImplify hello route

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,5 +1,5 @@
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from '@services/app.service';
 @ApiTags('Test route')
@@ -7,15 +7,8 @@ import { AppService } from '@services/app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:lang')
+  @Get('hello')
   @ApiOperation({ summary: 'Test routing with an "Hello World"' })
-  @ApiParam({
-    allowEmptyValue: true,
-    description:
-      "The language of the 'hello'. Default is english. 'ru' will send an error. ",
-    enum: ['fr', 'en', 'ru'],
-    name: 'lang',
-  })
   @ApiResponse({ description: 'OK', status: 200 })
   getHello(): Record<string, string> {
     return this.appService.getHello();
--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,7 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-
-import ApiError from '@shared/api-error';
-import { ErrorCodes } from '@interfaces/error.interface/error.interface';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
