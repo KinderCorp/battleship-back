29c06c19c504ee3b822b802d8afcc97abba5b953
Flavian Engevin <flavian.engevin@ynov.com>
2022-11-24 09:35:38 +0100

    Fix after rebase

--- src/domains/game/game.service.ts
+++ src/domains/game/game.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { CreateGameDto } from '@dto/game.dto';
@@ -13,6 +13,12 @@ export default class GameService {
   ) {}
 
   public async insert(game: CreateGameDto): Promise<Game> {
+    if (!game.winner && !game.loser) {
+      throw new BadRequestException(
+        'Please enter at least a winner or a loser',
+      );
+    }
+
     return this.gameRepository.insert(game);
   }
 }
