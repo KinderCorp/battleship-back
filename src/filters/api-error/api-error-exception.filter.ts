f6fbe44259cf6f5b25bb7e463cb306a5c84ad796
Flavian Engevin <flavian.engevin@ynov.com>
2022-11-26 17:34:03 +0100

    Remove CI warns

--- src/filters/api-error/api-error-exception.filter.ts
+++ src/filters/api-error/api-error-exception.filter.ts
@@ -6,13 +6,13 @@ import {
 } from '@nestjs/common';
 import { FastifyReply } from 'fastify';
 
-import { ApiErrorCodes } from '@interfaces/error.interface';
+// import { ApiErrorCodes } from '@interfaces/error.interface';
 
-type ErrorObject = {
-  code: ApiErrorCodes;
-  message: string | object;
-  title: string | object;
-};
+// type ErrorObject = {
+//   code: ApiErrorCodes;
+//   message: string | object;
+//   title: string | object;
+// };
 
 // FIXME This is not working properly
 @Catch(HttpException)
@@ -24,7 +24,7 @@ export default class ApiErrorExceptionFilter<T extends HttpException>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const response: FastifyReply<any> = ctx.getResponse<FastifyReply>();
     const status = exception.getStatus();
-    const exceptionResponse = exception.getResponse();
+    // const exceptionResponse = exception.getResponse();
 
     response.status(status).send(exception);
   }
