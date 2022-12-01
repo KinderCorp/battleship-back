71ec356616e8db624477e8c9659a650d39f8a2ce
Maxime <maxime.warluzel@ynov.com>
2022-12-01 11:02:57 +0100

    Fix format

--- src/engine/gateways/gameGateway.ts
+++ src/engine/gateways/gameGateway.ts
@@ -1,9 +1,16 @@
-import { MessageBody, OnGatewayConnection , SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
 import { Socket, Server as SocketServer } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ cors: { origin: '*' } })
-export class GameGateway implements OnGatewayConnection  {
+export class GameGateway implements OnGatewayConnection {
   public logger = new Logger();
 
   @WebSocketServer()
@@ -11,7 +18,7 @@ export class GameGateway implements OnGatewayConnection  {
 
   public handleConnection(socket: Socket) {
     this.logger.log(`Socket ${socket.id} connected`);
-    this.socketServer.emit('connected', 'User bien reçu')
+    this.socketServer.emit('connected', 'User bien reçu');
   }
   public handleDisconnect(socket: Socket) {
     this.logger.log(`Socket ${socket.id} disconnected`);
@@ -20,6 +27,6 @@ export class GameGateway implements OnGatewayConnection  {
   @SubscribeMessage('UserJoin')
   public onNewMessage(@MessageBody() body: string) {
     this.logger.log(body);
-    this.socketServer.emit('UserReceived', 'User bien reçu')
+    this.socketServer.emit('UserReceived', 'User bien reçu');
   }
 }
