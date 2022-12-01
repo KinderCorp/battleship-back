dc9d9d47fe982caf2ff1509ee9940b21d823c2ec
Maxime <maxime.warluzel@ynov.com>
2022-12-01 11:00:17 +0100

    Add web sockets

--- src/engine/gateways/gameGateway.ts
+++ src/engine/gateways/gameGateway.ts
@@ -1,17 +1,25 @@
-import { Logger, OnModuleInit } from '@nestjs/common';
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server as SocketServer } from 'socket.io';
+import { MessageBody, OnGatewayConnection , SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+import { Socket, Server as SocketServer } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ cors: { origin: '*' } })
-export class GameGateway implements OnModuleInit {
+export class GameGateway implements OnGatewayConnection  {
   public logger = new Logger();
 
   @WebSocketServer()
   public socketServer: SocketServer;
 
-  public onModuleInit() {
-    this.socketServer.on('connection', (socket) => {
-      this.logger.log(`Socket ${socket.id} connected`);
-    });
+  public handleConnection(socket: Socket) {
+    this.logger.log(`Socket ${socket.id} connected`);
+    this.socketServer.emit('connected', 'User bien reçu')
+  }
+  public handleDisconnect(socket: Socket) {
+    this.logger.log(`Socket ${socket.id} disconnected`);
+  }
+
+  @SubscribeMessage('UserJoin')
+  public onNewMessage(@MessageBody() body: string) {
+    this.logger.log(body);
+    this.socketServer.emit('UserReceived', 'User bien reçu')
   }
 }
